da97b6af42e1fb751a13fbc4e46a522146e2999d
abdali <abdaliidazzi1@gmail.com>
2024-08-21 12:32:57 +0100

    update3

--- master.js
+++ master.js
@@ -8,11 +8,45 @@ function menu() {
     line2Width.classList.toggle("line2Width");
 }
 //display overlay
-const searchInput = document.querySelector(".search-contener")
+/*const searchInput = document.querySelector(".search-contener")
 const overlay = document.querySelector(".overlay")
 searchInput.onfocus = function() {
     overlay.classList.add("overlayShow")
 }
 searchInput.onblur = function() {
     overlay.classList.remove("overlayShow")
+}*/
+let input = document.querySelector(".searchBox");
+let autoComplate = [
+    "abdali",
+    "kawtar",
+    "hakima",
+    "java script",
+    "HTML",
+    "CSS",
+    "how to make a website",
+    "how to fix your PC",
+    "lamar",
+    "bangladish",
+    
+];
+const div = document.querySelector(".list");
+input.onkeyup = function() {
+    let result = [];
+    let inputValue = input.value;
+    if (inputValue.length) {
+        result = autoComplate.filter((keyword) => {
+            return keyword.toLowerCase().includes(inputValue.toLowerCase());
+        });
+    }
+    display(result);
+}
+function display(result) {
+    let content = result.map((list) => {
+        return `<li onclick="displayOnInput(this)"> ${list}</li>`;
+    });
+    div.innerHTML = "<ul>"+content.join("")+"</ul>"
+}
+function displayOnInput(list) {
+    input.value = list.innerHTML
 }
